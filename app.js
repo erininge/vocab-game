65ce34a738129c7f62b2c07cf3856ef1f7fc8a52
erininge <eriningebritson@gmail.com>
2026-01-23 22:40:03 -0600

    Bump app version and cache keys

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
    - Lessons follow the python structure: { level, lessons: { "1": [ {kana, kanji, en:[...], ...}, ... ] } }
 */
 
-const APP_VERSION = "v0.3.50";
+const APP_VERSION = "v0.3.51";
 const STAR_STORAGE_KEY = "vocabGardenStarred";
 const AUDIO_VOICE_DEFAULT = "Female option 1";
 const FIXED_AUDIO_VOLUME = 2.5;
